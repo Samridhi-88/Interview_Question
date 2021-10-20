3fe0e7337782b6d1a54d02f04904f3b4e9836463
Samridhi-88 <80439205+Samridhi-88@users.noreply.github.com>
2021-10-20 10:38:29 +0530

    Add files via upload

---
+++ shipping.py
@@ -0,0 +1,93 @@
+country=input("enter your country name")
+product_code=input("enter your product code")
+weight=float(input("enter your weight in kg"))
+carancy=input("enter the curancy")
+if country=="UK":
+    if weight<5:
+        shipping_cost=5
+        if product_code[0:3]=="123":
+            if carancy=="US" or carancy=="us":
+                cost_1=shipping_cost*1.2*(1+0.18)
+                cost=cost_1/80
+                print(cost,"us dollor")
+            else:
+                cost_1=shipping_cost*1.2*(1+0.18)
+                print(cost_1)
+        elif product_code[0:3]=="234":
+            if carancy=="US" or carancy=="us":
+                cost_1=shipping_cost*1.2*(1+0.18)
+                cost=cost_1/80
+                print(cost,"us dollor")
+            else:
+                cost_1=shipping_cost*1.2*(1+0.18)
+                print(cost_1)
+        else:
+            cost_1=shipping_cost*(1+0.18)
+            print(cost_1)
+    elif weight>=5:
+        shipping_cost=15
+        if product_code[0:3]=="123":
+            if carancy=="US" or carancy=="us":
+                cost_2=shipping_cost*1.2*(1+0.18)
+                cost=cost_2/80
+                print(cost,"dollor")
+            else:
+                cost_2=shipping_cost*1.2*(1+0.18)
+                print(cost_2)
+        elif product_code[0:3]=="234":
+            if carancy=="US" or carancy=="us":
+                cost_2=shipping_cost*1.5*(1+0.18)
+                cost=cost_2/80
+                print(cost,"dollor")
+            else:
+                cost_2=shipping_cost*1.2*(1+0.18)
+                print(cost_2)
+        else:
+            cost_2=shipping_cost*(1+0.18)
+            print(cost_2)
+elif country=="US":
+    if weight<10:
+        shipping_cost=5
+        if product_code[0:3]=="123":
+            if carancy=="US" or carancy=="us":
+                cost_3=shipping_cost*1.2*(1+0.18)
+                cost=cost_3/80
+                print(cost,"dollor")
+            else:
+                cost_3=shipping_cost*1.2*(1+0.18)
+                print(cost_3)
+        elif  product_code[0:3]=="234":
+            if carancy=="US" or carancy=="us":
+                cost_3=shipping_cost*1.5*(1+0.18)
+                cost=cost_3/80
+                print(cost,"dollor")
+            else:
+                cost_3=shipping_cost*1.2*(1+0.18)
+                print(cost_3)
+                print("your carency is not matching")
+        else:
+            cost_3=shipping_cost*(1+0.18)
+            print(cost_3)
+    elif weight>=10:
+        shipping_cost=15
+        if product_code[0:3]=="123":
+            if carancy=="US" or carancy=="us":
+                cost_4=shipping_cost*1.2*(1+0.18)
+                cost=cost_4/80
+                print(cost,"dollor")
+            else:
+                cost_4=shipping_cost*1.2*(1+0.18)
+                print(cost_4)
+        elif product_code[0:3]=="234":
+            if carancy=="US" or carancy=="us":
+                cost_4=shipping_cost*1.5*(1+0.18)
+                cost=cost_4/80
+                print(cost,"dollor")
+            else:
+                cost_4=shipping_cost*1.2*(1+0.18)
+                print(cost_4)
+        else:
+            cost_4=shipping_cost*(1+0.18)
+            print(cost_4)
+else:
+    print("Not applicable")
